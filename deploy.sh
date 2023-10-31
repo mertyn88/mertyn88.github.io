f4ae33d6f60d0ea33b962b8d819a9bdd7ba52607
mertyn88 <mertyn88@gmail.com>
2023-10-31 00:57:15 +0000

    + commit

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,34 @@
 #!/bin/bash
 
-# master commit
+branch_name="feature/$(date +%Y).$(date +%m).$(date +%d)"
+
+# check branch
+if git rev-parse --quiet --verify "refs/heads/$branch_name" > /dev/null; then
+  echo "exist $branch_name"
+else
+  # create branch
+  git checkout -b "$branch_name"
+fi
+
+# commit
 git add .
 git commit -m "$(date +%Y).$(date +%m).$(date +%d)"
-# master push
+# push
+git push --set-upstream origin $branch_name
+
+# master merge
+git checkout master
+git merge $branch_name
+
+# push master
 git push --set-upstream origin master
-# check out gh-pages
+
+# gh-pages merge
 git checkout gh-pages
-# merge master
-git merge master
+git merge $branch_name
+
 # push gh-pages
 git push --set-upstream origin gh-pages
 
+# checkout master
 git checkout master
