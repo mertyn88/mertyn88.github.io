69ebb8cc33f99419f3fba23bb1fbcfb7daeb8957
mertyn88 <mertyn88@gmail.com>
2023-10-31 06:12:20 +0000

    2023.10.31

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,7 @@ branch_name="feature/$(date +%Y).$(date +%m).$(date +%d)"
 # check branch
 if git rev-parse --quiet --verify "refs/heads/$branch_name" > /dev/null; then
   echo "exist $branch_name"
+  git checkout $branch_name
 else
   # create branch
   git checkout -b "$branch_name"
