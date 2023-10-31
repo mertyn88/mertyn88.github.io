1ba283adafd1ec1380fa87bae625980da8a28b37
mertyn88 <mertyn88@gmail.com>
2023-10-31 00:41:09 +0000

    2023.10.31

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
-git checkout gh-pages
-git merge master
+# master commit
 git add .
 git commit -m "$(date +%Y).$(date +%m).$(date +%d)"
+# master push
+git push --set-upstream origin master
+# check out gh-pages
+git checkout gh-pages
+# merge master
+git merge master
+# push gh-pages
 git push --set-upstream origin gh-pages
 
 git checkout master
